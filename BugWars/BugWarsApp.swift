e7fc4ba2120447d87ec94b0a0d4724666d2bc70a
Tom Phillips <tomphillipsmusic@gmail.com>
2023-03-30 09:53:03 -0400

    Initial Commit

---
+++ BugWars/BugWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugWarsApp.swift
+//  BugWars
+//
+//  Created by Tom Phillips on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct BugWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
